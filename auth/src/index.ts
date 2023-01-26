fd14a455fac53214c09907b92337c79965a2aa38
Tommy <tommyubongabasijohn@gmail.com>
2023-01-26 15:27:31 +0100

    initial routes scaffolding

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
+import { currentuserRouter } from './routes/current-user'
+import { signoutRouter } from './routes/signout'
+import { signinRouter } from './routes/signin'
+import { signupRouter } from './routes/signup'
 
 const app = express()
-
-
 app.use(express.json())
 
+app.use(currentuserRouter)
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Test response')
-})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
