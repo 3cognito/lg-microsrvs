340fa2fad80b1272f790902fd081263826f7a641
Tommy <tommyubongabasijohn@gmail.com>
2023-01-26 18:36:16 +0100

    routes and error middlewares passed to express app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,19 @@
-import express from 'express'
-import { currentuserRouter } from './routes/current-user'
-import { signoutRouter } from './routes/signout'
-import { signinRouter } from './routes/signin'
-import { signupRouter } from './routes/signup'
+import express from "express";
+import { currentuserRouter } from "./routes/current-user";
+import { signoutRouter } from "./routes/signout";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/error-handler";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(currentuserRouter)
-
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-})
+  console.log("Listening on port 3000!");
+});
