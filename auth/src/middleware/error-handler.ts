af49a713769ae48a418ef73e74a7ab17a92d02cb
Tommy <tommyubongabasijohn@gmail.com>
2023-01-27 19:39:23 +0100

    error handling middleware

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +7,11 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("An error occured", err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-  res.status(500).json({
-    message: err.message,
+  res.status(400).send({
+    errors: [{ message: "Something went wrong" }],
   });
 };
