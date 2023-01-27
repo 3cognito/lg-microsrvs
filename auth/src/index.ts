30f7ca162fd0aebc1e48fcba7d3101d527ea0073
Tommy <tommyubongabasijohn@gmail.com>
2023-01-27 19:41:52 +0100

    error handling middleware setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,19 +1,28 @@
 import express from "express";
-import { currentuserRouter } from "./routes/current-user";
-import { signoutRouter } from "./routes/signout";
+import "express-async-errors";
+import { json } from "body-parser";
+
+import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 
-app.use(currentuserRouter);
+app.use(currentUserRouter);
 app.use(signinRouter);
-app.use(signupRouter);
 app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+  console.log("Listening on port 3000!!!!!!!!");
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,31 @@
-import express from 'express'
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/req-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-errors";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/api/users/signup', (req, res) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-})
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
+    console.log("Creating a user...");
+    throw new DatabaseConnectionError();
 
-export {router as signupRouter}
+    res.send({});
+  }
+);
+
+export { router as signupRouter };
