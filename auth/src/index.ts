09bee38f8817147b03a62c708c8ef9c22ddee809
Tommy <tommyubongabasijohn@gmail.com>
2023-01-27 20:58:44 +0100

    db connect setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,6 +24,16 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("" /**connect to the mongo cluster ip */);
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
