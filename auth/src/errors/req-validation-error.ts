1e34aa18e0c780bb83708c0e82f8881d96f6992c
Tommy <tommyubongabasijohn@gmail.com>
2023-01-27 19:37:58 +0100

    invalid request body class

---
+++ auth/src/errors/req-validation-error.ts
@@ -0,0 +1,19 @@
+import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(public errors: ValidationError[]) {
+    super("Invalid request parameters");
+
+    //Due to extension of built in class - Lookup and compare to error stack ish - Personal note
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
+  }
+}
