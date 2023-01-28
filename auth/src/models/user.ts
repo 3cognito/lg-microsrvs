6fc799f3dc59aeec1ae80b9e89f85b641a8ca220
Tommy <tommyubongabasijohn@gmail.com>
2023-01-28 01:15:25 +0100

    user model interface

---
+++ auth/src/models/user.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+interface usersAttrs {
+  email: string;
+  password: string;
+}
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const buildUser = () => {};
+
+export { User };
